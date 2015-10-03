46613eca81cb0038c510f2d3d410421735f138e5
Saurabh Shukla <sshukla101@gmail.com>
2015-10-02 20:09:41 -0500

    modified clustring.py

--- adaptive_sampling/clustering.py
+++ adaptive_sampling/clustering.py
@@ -14,8 +14,8 @@ from sklearn.cluster import KMeans
 myn_clusters = 200
 from_clusters=5
 frame_per_cluster=10
-trj="apo_aba_with_water.mdcrd"
-top="apo_aba_with_water.top"
+traj_name='apo_aba_with_water.mdcrd'
+top_name='apo_aba.top'
 
 ################################################################
 #If we are using distance of the residues as the metric
@@ -59,15 +59,14 @@ frames=np.array(frames).reshape(from_clusters*frame_per_cluster)
 cpp_frm=frames+1
 
 for i in range(len(cpp_frm)):
-	f = open('restart_'+ str(i), 'w')
-	f.write('parm ' + 'apo_aba.top' + '\n')
-	f.write('trajin ' + 'apo_aba_with_water.mdcrd'  + '\n')
+	f = open('res_cpptraj'+ str(i+1) + '.in', 'w')
+	f.write('parm ' + top_name  + '\n')
+	f.write('trajin ' + traj_name  + '\n')
 	f.write('parmbox alpha 90 beta 90 gamma 90\n')
-	f.write('trajout Sample_' + str(i) + '.rst restart onlyframes ' + str(cpp_frm[i]) +'\n')
+	f.write('trajout par' + str(i+1) + '_sim0'+ '.rst restart onlyframes ' + str(cpp_frm[i]) +'\n')
 	f.write('run \n')
 	f.write('quit \n')
 	f.close()
-
 
 #Extracting one restart file from 189 GB dataset takes 1 hour 
 
