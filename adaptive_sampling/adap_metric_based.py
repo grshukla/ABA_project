131b795e97f7754b12f06021871509f14e9b4e16
Saurabh Shukla <sshukla101@gmail.com>
2015-10-03 20:51:23 -0500

    Metric based adaptive sampling

--- adaptive_sampling/adap_metric_based.py
+++ adaptive_sampling/adap_metric_based.py
@@ -11,17 +11,19 @@ from sklearn.cluster import KMeans
 ##############################################################
 #User defined variables
 ##############################################################
-myn_clusters = 200
-from_clusters=5
-frame_per_cluster=10
-traj_name='apo_aba_with_water.mdcrd'
+myn_clusters = 200                       # Total number of clusters
+from_clusters=5                          # Restart files making from clusters 
+frame_per_cluster=10                     # Number of frames extracted from each cluster
+traj_name='apo_aba_with_water.mdcrd'     
 top_name='apo_aba.top'
+path='/home/sshukla4/scratch/newsim/pyl10/apo_aba/prod/round1/combined_traj'
+d_file_name='lys_aba_distance.dat'
 
 ################################################################
 #If we are using distance of the residues as the metric
 ################################################################
 
-temp=pd.read_csv('lys_aba_distance.dat', sep='\s+')
+temp=pd.read_csv(d_file_name, sep='\s+')
 
 dataset=temp.iloc[:,1]
 dataset=np.array(dataset)
@@ -58,6 +60,10 @@ for i in range(from_clusters):
 frames=np.array(frames).reshape(from_clusters*frame_per_cluster)
 cpp_frm=frames+1
 
+
+################################################################
+#Writing cpptraj input files
+################################################################
 for i in range(len(cpp_frm)):
 	f = open('res_cpptraj'+ str(i+1) + '.in', 'w')
 	f.write('parm ' + top_name  + '\n')
@@ -70,7 +76,28 @@ for i in range(len(cpp_frm)):
 
 #Extracting one restart file from 189 GB dataset takes 1 hour 
 
+################################################################
+#PBS Scripts 
+################################################################
+for i in range(len(cpp_frm)):
+        f = open('res_'+ str(i+1) + '.pbs', 'w')
+        f.write('#!/bin/bash'+ '\n')
+        f.write('#PBS -l walltime=04:00:00'+ '\n')
+        f.write('#PBS -l nodes=1:ppn=1'+ '\n')
+        f.write('#PBS -N rst_'+ str(i+1)+ '\n')
+        f.write('#PBS -q secondary'+ '\n')
+        f.write('#PBS -W group_list=chbe_diwakar'+ '\n')
+	f.write('cd '+ path+ '\n')
+	f.write('cpptraj -i res_cpptraj' + str(i+1) + '.in'+ '\n')
+        f.close()
 
 
-
- 
+################################################################
+#Submission Script       
+################################################################
+f = open('submission_rst.sh', 'w')
+f.write('#!/bin/bash'+ '\n')
+f.write('for k in $(seq 1 '+ str(len(cpp_frm)) +'); do'+ '\n')
+f.write('	qsub res_${i}.pbs'+ '\n')
+f.write('done'+ '\n')
+f.close()
