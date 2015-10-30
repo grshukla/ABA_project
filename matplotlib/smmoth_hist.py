7bb5b488024657e636073b4596f9caa29af58c3b
Saurabh Shukla <sshukla101@gmail.com>
2015-10-30 11:33:15 -0500

    Drug update

--- matplotlib/smmoth_hist.py
+++ matplotlib/smmoth_hist.py
@@ -16,7 +16,7 @@ g.close()
 plt.subplot(2,3,1)
 sns.distplot(prop_matrix[:,0], color="lightskyblue",bins=20, kde=True, kde_kws={"color": "k"})
 plt.xlim(0,1000)
-plt.xlabel('MW',fontsize=20)
+plt.xlabel('$\mathbf{M_r}$',fontsize=20)
 plt.ylabel('Density',fontsize=20)
 plt.xticks(fontsize = 15)
 plt.yticks(fontsize = 15)
@@ -24,7 +24,7 @@ plt.yticks(fontsize = 15)
 plt.subplot(2,3,2)
 sns.distplot(prop_matrix[:,1], color="lightskyblue",bins=300, kde=True, kde_kws={"color": "k","bw": "0.1"})
 plt.xlim(-8,10)
-plt.xlabel('Log(P)',fontsize=20)
+plt.xlabel('ALOGP',fontsize=20)
 plt.xticks(fontsize = 15)
 plt.yticks(fontsize = 15)
 
@@ -36,38 +36,42 @@ plt.xticks(fontsize = 15)
 plt.yticks(fontsize = 15)
 
 plt.subplot(2,3,4)
-sns.distplot(prop_matrix[:,3], color="lightskyblue",bins=20, kde=True, kde_kws={"color": "k", "bw":"0.5"})
-plt.xlim(0,18)
-plt.xlabel('HBA',fontsize=20)
+sns.distplot(prop_matrix[:,4], color="lightskyblue",bins=30, kde=True, kde_kws={"color": "k"})
+plt.xlim(0,300)
+plt.xlabel('PSA',fontsize=20)
 plt.ylabel('Density',fontsize=20)
 plt.xticks(fontsize = 15)
 plt.yticks(fontsize = 15)
 
 plt.subplot(2,3,5)
-sns.distplot(prop_matrix[:,4], color="lightskyblue",bins=30, kde=True, kde_kws={"color": "k"})
-plt.xlim(0,300)
-plt.xlabel('PSA',fontsize=20)
-plt.xticks(fontsize = 15)
-plt.yticks(fontsize = 15)
-
-plt.subplot(2,3,6)
 sns.distplot(prop_matrix[:,5], color="lightskyblue",bins=70, kde=True, kde_kws={"color": "k"})
 plt.xlim(0,25)
 plt.xlabel('ROTB',fontsize=20)
 plt.xticks(fontsize = 15)
 plt.yticks(fontsize = 15)
 
-plt.show()
-
-######################NEW PROPS#######################
-plt.subplot(2,3,1)
+plt.subplot(2,3,6)
 sns.distplot(smiles_matrix[:,0], color="lightskyblue",bins=10, kde=True, kde_kws={"color": "k", "bw" : "0.5"})
 plt.xlim(0,5)
-plt.xlabel('# of Arom rings',fontsize=20)
-plt.ylabel('Density',fontsize=20)
+plt.xlabel('AROM',fontsize=20)
 plt.xticks(fontsize = 15)
 plt.yticks(fontsize = 15)
 
+plt.show()
+
+
+
+
+
+
+######################NEW PROPS#######################
+plt.subplot(2,3,1)
+sns.distplot(prop_matrix[:,3], color="lightskyblue",bins=20, kde=True, kde_kws={"color": "k", "bw":"0.5"})
+plt.xlim(0,18)
+plt.xlabel('HBA',fontsize=20)
+plt.ylabel('Density',fontsize=20)
+plt.xticks(fontsize = 15)
+plt.yticks(fontsize = 15)
 
 plt.subplot(2,3,2)
 sns.distplot(smiles_matrix[:,6], color="lightskyblue",bins=10, kde=True, kde_kws={"color": "k","bw": "0.5"})
@@ -107,3 +111,9 @@ plt.xticks(fontsize = 15)
 plt.yticks(fontsize = 15)
 
 plt.show()
+
+
+####################### Cmparison of HBA and HBD ###################
+
+
+
