05e88992cc6a6013aa990288ee2cb095ef8040d0
Saurabh Shukla <sshukla101@users.noreply.github.com>
2015-11-13 12:44:41 -0600

    Update ChemBL_drugs.py

--- drug_similarity/ChemBL_drugs.py
+++ drug_similarity/ChemBL_drugs.py
@@ -6,7 +6,14 @@ import os
 from matplotlib.colors import LogNorm
 
 
-f1='chembl_drugs.txt'
-temp= pd.read_table(f1)
-oral_drugs=temp[temp.ORAL=='Y']
-oral_drugs=oral_drugs.dropna(subset=['CANONICAL_SMILES'])
+f1='chembl_drugs.txt'               #File name 
+temp= pd.read_table(f1)            
+oral_drugs=temp[temp.ORAL=='Y']     #Retaining only orally administered drugs
+oral_drugs=oral_drugs.dropna(subset=['CANONICAL_SMILES'])   #Removing the columns which are "NaN" for smiles
+
+#Loading the dataset
+g= open('oral_drugs.pkl', 'rb')
+oral_drugs=pkl.load(g)
+g.close()
+
+
