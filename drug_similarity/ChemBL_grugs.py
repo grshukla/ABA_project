553ee148059c169362c429d40e39395b607b73c4
Saurabh Shukla <sshukla101@users.noreply.github.com>
2015-11-13 12:15:57 -0600

    Create ChemBL_grugs.py

---
+++ drug_similarity/ChemBL_grugs.py
@@ -0,0 +1,10 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import pandas as pd
+import cPickle as pkl
+import os
+from matplotlib.colors import LogNorm
+
+temp= pd.read_table(f1)
+oral_drugs=temp[temp.ORAL=='Y']
+oral_drugs=oral_drugs.dropna(subset=['CANONICAL_SMILES'])
