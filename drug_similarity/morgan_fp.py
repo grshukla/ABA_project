b990cf9ef3d9275c54a0a4a82f4b1bbb2463cabd
Saurabh Shukla <sshukla101@users.noreply.github.com>
2015-11-16 19:32:34 -0600

    Update morgan_fp.py

--- drug_similarity/morgan_fp.py
+++ drug_similarity/morgan_fp.py
@@ -1,5 +1,6 @@
 from rdkit import Chem
 from rdkit.Chem import AllChem
+import numpy as np
 
 #Finding the Noraml fingerptirint
 m1 = Chem.MolFromSmiles('Cc1ccccc1')
@@ -12,5 +13,16 @@ fp1 = AllChem.GetMorganFingerprintAsBitVect(m1,2,nBits=1024)
 n_on_bits=fp1.GetNumOnBits()          #number of on bits
 on_bits=fp1.GetOnBits()               #Index of on bits
 
+#Making a Simple Neural Net
+#Making the dataset
+x_data=np.zeros((len(smiles),1024))
+for i in range(6):
+  m1 = Chem.MolFromSmiles(smiles[i])
+  fp1 = AllChem.GetMorganFingerprintAsBitVect(m1,2,nBits=1024)
+  n_on_bits=fp1.GetNumOnBits() 
+  on_bits=fp1.GetOnBits()
+  for j in range(n_on_bits):
+    x_data[i,on_bits[j]]=1
+    
 
 
