c18dbe93e4c1e617c6b4e50b9bf1f98c930a7654
Saurabh Shukla <sshukla101@users.noreply.github.com>
2015-11-16 20:02:32 -0600

    Update morgan_fp.py

--- drug_similarity/morgan_fp.py
+++ drug_similarity/morgan_fp.py
@@ -25,4 +25,25 @@ for i in range(6):
     x_data[i,on_bits[j]]=1
     
 
+y_data=np.zeros((6,2))
+
+#Making a Neural Net
+x = tf.placeholder("float", shape=[None, 1024])   #Number of features =1024
+y_ = tf.placeholder("float", shape=[None, 2])     # Output can be either 0 or 1
+
+W = tf.Variable(tf.zeros([1024,2]))
+b = tf.Variable(tf.zeros([2]))
+
+sess.run(tf.initialize_all_variables())
+y = tf.nn.softmax(tf.matmul(x,W) + b)
+
+cross_entropy = -tf.reduce_sum(y_*tf.log(y))
+
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
+
+for i in range(1000):
+  train_step.run(feed_dict={x: x_data, y_: y_data})
+
+
+
 
