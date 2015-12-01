9aefd143c812c109a98c60d89fd3daeeffd89aa1
Saurabh Shukla <sshukla101@users.noreply.github.com>
2015-12-01 17:17:11 -0600

    Create feature_vector.py

---
+++ drug_similarity/feature_vector.py
@@ -0,0 +1,14 @@
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import Descriptors
+from rdkit.Chem import Fragments
+from rdkit.Chem import GraphDescriptors
+
+#http://www.rdkit.org/Python_Docs/rdkit.Chem.Fragments-module.html
+#number of functions groups 
+
+#topological complexity
+#http://rdkit.org/Python_Docs/rdkit.Chem.GraphDescriptors-module.html#_VertexDegrees
+
+#Lipinski Descriptors
+#http://rdkit.org/Python_Docs/rdkit.Chem.Lipinski-module.html
