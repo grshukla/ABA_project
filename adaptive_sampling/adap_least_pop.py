b8c5ccb8c9eb45bd47b957a36ce68f6fd9e1fb9b
Saurabh Shukla <sshukla101@users.noreply.github.com>
2015-12-11 16:53:37 -0600

    Update adap_least_pop.py

--- adaptive_sampling/adap_least_pop.py
+++ adaptive_sampling/adap_least_pop.py
@@ -10,13 +10,13 @@ import pandas as pd
 ##############################################################
 rec='pyl2'
 sys='holo_aba'
-myn_clusters = 200                       # Total number of clusters
+myn_clusters =200                       # Total number of clusters
 from_clusters=50                          # Restart files making from clusters 
 frame_per_cluster=1                     # Number of frames extracted from each cluster
-traj_name='holo_aba_with_water.mdcrd'     
-top_name='holo_aba.top'
-path='/home/sshukla4/scratch/newsim/pyl2/holo_aba/prod/round1/combined_traj'
-d_file_name='loop_rmsd_pyl2_holo.dat'
+traj_name='pyl5_apo_aba_r1_combined.mdcrd'     
+top_name='apo_aba.top'
+path='/scratch/users/sshukla4/newsim/pyl5/apo_aba/prod/round1/analysis/round3_rst'
+d_file_name='pyl5_apo_distance_r1.dat'
 
 
 ################################################################
@@ -65,39 +65,39 @@ f.close()
 ################################################################
 #Writing cpptraj input files
 ################################################################
+f = open('res_cpptraj.in', 'wb')
+f.write('parm ' + top_name  + '\n')
+f.write('trajin ' + traj_name  + '\n')
+f.write('parmbox alpha 90 beta 90 gamma 90\n')
 for i in range(len(cpp_frm)):
-	f = open('res_cpptraj'+ str(i+1) + '.in', 'w')
-	f.write('parm ' + top_name  + '\n')
-	f.write('trajin ' + traj_name  + '\n')
-	f.write('parmbox alpha 90 beta 90 gamma 90\n')
 	f.write('trajout par' + str(i+1) + '_sim0'+ '.rst restart onlyframes ' + str(cpp_frm[i]) +'\n')
-	f.write('run \n')
-	f.write('quit \n')
-	f.close()
+f.write('run \n')
+f.write('quit \n')
+f.close()
 
 #Extracting one restart file from 189 GB dataset takes 1 hour 
 
 ################################################################
 #PBS Scripts 
 ################################################################
-for i in range(len(cpp_frm)):
-        f = open('res_'+ str(i+1) + '.pbs', 'w')
-        f.write('#!/bin/bash'+ '\n')
-        f.write('#PBS -l walltime=04:00:00'+ '\n')
-        f.write('#PBS -l nodes=1:ppn=1'+ '\n')
-        f.write('#PBS -N rst_'+ str(i+1)+ '\n')
-        f.write('#PBS -q secondary'+ '\n')
-        f.write('#PBS -W group_list=chbe_diwakar'+ '\n')
-	f.write('cd '+ path+ '\n')
-	f.write('cpptraj -i res_cpptraj' + str(i+1) + '.in'+ '\n')
-        f.close()
+#for i in range(len(cpp_frm)):
+#        f = open('res_'+ str(i+1) + '.pbs', 'w')
+#        f.write('#!/bin/bash'+ '\n')
+#        f.write('#PBS -l walltime=04:00:00'+ '\n')
+#        f.write('#PBS -l nodes=1:ppn=1'+ '\n')
+#        f.write('#PBS -N rst_'+ str(i+1)+ '\n')
+ #       f.write('#PBS -q secondary'+ '\n')
+  #      f.write('#PBS -W group_list=chbe_diwakar'+ '\n')
+#	f.write('cd '+ path+ '\n')
+#	f.write('cpptraj -i res_cpptraj' + str(i+1) + '.in'+ '\n')
+ #       f.close()
 
 ################################################################
 #Submission Script       
 ################################################################
-f = open('submission_rst.sh', 'w')
-f.write('#!/bin/bash'+ '\n')
-f.write('for i in $(seq 1 '+ str(len(cpp_frm)) +'); do'+ '\n')
-f.write('	qsub res_${i}.pbs'+ '\n')
-f.write('done'+ '\n')
-f.close()
+#f = open('submission_rst.sh', 'w')
+#f.write('#!/bin/bash'+ '\n')
+#f.write('for i in $(seq 1 '+ str(len(cpp_frm)) +'); do'+ '\n')
+#f.write('	qsub res_${i}.pbs'+ '\n')
+#f.write('done'+ '\n')
+#f.close()
